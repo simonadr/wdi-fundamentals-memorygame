92690c3bcda625339c8b7dbfa04e00313fef9b78
Simon Adriaenssens <simon.a19954@hotmail.fr>
2019-08-19 17:28:19 +0200

    Update main.js

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,33 @@
 console.log("Up and running");
 
 var cards = ["queen", "queen", "king", "king"];
-var cardOne = cards[0];
 var cardsInPlay = [];
-cardsInPlay.push("cardOne");
-cardsInPlay.push("cardTwo");
-console.log("User flipped queen");
 
-var cardTwo = cards[1];
-console.log("User flipped king");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
 
-if (cardsInPlay.length === 2) {
+function flipCard(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(card[0]);
+	if (cardsInPlay.length === 1) {
+		checkForMatch();
+	}
+}
+
+flipCard(0);
+flipCard(2);
+checkForMatch();
+
+if (cardsInPlay.length === 1) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
 	} else {
 		console.log("Sorry, try again");
 	}
 }
+
